d64616264a57ab1e43cf44688d38c16fc9ab38ef
VintusS <dragomir.mindrescu@isa.utm.md>
2023-07-09 15:26:43 +0300

    Button 6 disconnection fix

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -34,25 +34,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        clearButtonView.layer.cornerRadius = clearButtonView.frame.height / 2
-        plusMinusrButtonView.layer.cornerRadius = plusMinusrButtonView.frame.height / 2
-        percentagerButtonView.layer.cornerRadius = percentagerButtonView.frame.height / 2
-        divisionrButtonView.layer.cornerRadius = divisionrButtonView.frame.height / 2
-        multiplicationrButtonView.layer.cornerRadius = multiplicationrButtonView.frame.height / 2
-        minusrButtonView.layer.cornerRadius = minusrButtonView.frame.height / 2
-        plusrButtonView.layer.cornerRadius = plusrButtonView.frame.height / 2
-        equalrButtonView.layer.cornerRadius = equalrButtonView.frame.height / 2
-        zerorButtonView.layer.cornerRadius = zerorButtonView.frame.height / 2
-        commarButtonView.layer.cornerRadius = commarButtonView.frame.height / 2
-        onerButtonView.layer.cornerRadius = onerButtonView.frame.height / 2
-        tworButtonView.layer.cornerRadius = tworButtonView.frame.height / 2
-        threerButtonView.layer.cornerRadius = threerButtonView.frame.height / 2
-        fourrButtonView.layer.cornerRadius = fourrButtonView.frame.height / 2
-        fiverButtonView.layer.cornerRadius = fiverButtonView.frame.height / 2
-        sixrButtonView.layer.cornerRadius = sixrButtonView.frame.height / 2
-        sevenrButtonView.layer.cornerRadius = sevenrButtonView.frame.height / 2
-        eightrButtonView.layer.cornerRadius = eightrButtonView.frame.height / 2
-        ninerButtonView.layer.cornerRadius = ninerButtonView.frame.height / 2
+//        clearButtonView.layer.cornerRadius = clearButtonView.frame.height / 2
+//        plusMinusrButtonView.layer.cornerRadius = plusMinusrButtonView.frame.height / 2
+//        percentagerButtonView.layer.cornerRadius = percentagerButtonView.frame.height / 2
+//        divisionrButtonView.layer.cornerRadius = divisionrButtonView.frame.height / 2
+//        multiplicationrButtonView.layer.cornerRadius = multiplicationrButtonView.frame.height / 2
+//        minusrButtonView.layer.cornerRadius = minusrButtonView.frame.height / 2
+//        plusrButtonView.layer.cornerRadius = plusrButtonView.frame.height / 2
+//        equalrButtonView.layer.cornerRadius = equalrButtonView.frame.height / 2
+//        zerorButtonView.layer.cornerRadius = zerorButtonView.frame.height / 2
+//        commarButtonView.layer.cornerRadius = commarButtonView.frame.height / 2
+//        onerButtonView.layer.cornerRadius = onerButtonView.frame.height / 2
+//        tworButtonView.layer.cornerRadius = tworButtonView.frame.height / 2
+//        threerButtonView.layer.cornerRadius = threerButtonView.frame.height / 2
+//        fourrButtonView.layer.cornerRadius = fourrButtonView.frame.height / 2
+//        fiverButtonView.layer.cornerRadius = fiverButtonView.frame.height / 2
+//        sixrButtonView.layer.cornerRadius = sixrButtonView.frame.height / 2
+//        sevenrButtonView.layer.cornerRadius = sevenrButtonView.frame.height / 2
+//        eightrButtonView.layer.cornerRadius = eightrButtonView.frame.height / 2
+//        ninerButtonView.layer.cornerRadius = ninerButtonView.frame.height / 2
         resultLabelOutlet.text =  ""
     }
     
