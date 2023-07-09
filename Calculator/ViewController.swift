dc886098fabbe529eb7cb4167d3168356e9df44f
VintusS <dragomir.mindrescu@isa.utm.md>
2023-07-09 13:20:01 +0300

    Connecting Main.storyboard with ViewController. FIxing button placement

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var resultLabelOutlet: UILabel!
+    @IBOutlet weak var clearButtonView: UIButton!
+    @IBOutlet weak var plusMinusrButtonView: UIButton!
+    @IBOutlet weak var percentagerButtonView: UIButton!
+    @IBOutlet weak var divisionrButtonView: UIButton!
+    @IBOutlet weak var multiplicationrButtonView: UIButton!
+    @IBOutlet weak var minusrButtonView: UIButton!
+    @IBOutlet weak var plusrButtonView: UIButton!
+    @IBOutlet weak var equalrButtonView: UIButton!
+    @IBOutlet weak var zerorButtonView: UIButton!
+    @IBOutlet weak var commarButtonView: UIButton!
+    @IBOutlet weak var onerButtonView: UIButton!
+    @IBOutlet weak var tworButtonView: UIButton!
+    @IBOutlet weak var threerButtonView: UIButton!
+    @IBOutlet weak var fourrButtonView: UIButton!
+    @IBOutlet weak var fiverButtonView: UIButton!
+    @IBOutlet weak var sixrButtonView: UIButton!
+    @IBOutlet weak var sevenrButtonView: UIButton!
+    @IBOutlet weak var eightrButtonView: UIButton!
+    @IBOutlet weak var ninerButtonView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
