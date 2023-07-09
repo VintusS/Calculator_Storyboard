c4f261e007ef35e3441c17004cee4fc236d935d5
VintusS <dragomir.mindrescu@isa.utm.md>
2023-07-09 21:20:52 +0300

    Adapt the interface for my iPad Air 5th gen

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -30,35 +30,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var eightrButtonView: UIButton!
     @IBOutlet weak var ninerButtonView: UIButton!
     
-    var resultLabelValue: Double?
+    var value0: Double = 0
+    var value1: Double = 1
+    var comma0: Bool = false
+    var comma1: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        clearButtonView.layer.cornerRadius = clearButtonView.frame.height / 2
-//        plusMinusrButtonView.layer.cornerRadius = plusMinusrButtonView.frame.height / 2
-//        percentagerButtonView.layer.cornerRadius = percentagerButtonView.frame.height / 2
-//        divisionrButtonView.layer.cornerRadius = divisionrButtonView.frame.height / 2
-//        multiplicationrButtonView.layer.cornerRadius = multiplicationrButtonView.frame.height / 2
-//        minusrButtonView.layer.cornerRadius = minusrButtonView.frame.height / 2
-//        plusrButtonView.layer.cornerRadius = plusrButtonView.frame.height / 2
-//        equalrButtonView.layer.cornerRadius = equalrButtonView.frame.height / 2
-//        zerorButtonView.layer.cornerRadius = zerorButtonView.frame.height / 2
-//        commarButtonView.layer.cornerRadius = commarButtonView.frame.height / 2
-//        onerButtonView.layer.cornerRadius = onerButtonView.frame.height / 2
-//        tworButtonView.layer.cornerRadius = tworButtonView.frame.height / 2
-//        threerButtonView.layer.cornerRadius = threerButtonView.frame.height / 2
-//        fourrButtonView.layer.cornerRadius = fourrButtonView.frame.height / 2
-//        fiverButtonView.layer.cornerRadius = fiverButtonView.frame.height / 2
-//        sixrButtonView.layer.cornerRadius = sixrButtonView.frame.height / 2
-//        sevenrButtonView.layer.cornerRadius = sevenrButtonView.frame.height / 2
-//        eightrButtonView.layer.cornerRadius = eightrButtonView.frame.height / 2
-//        ninerButtonView.layer.cornerRadius = ninerButtonView.frame.height / 2
+        clearButtonView.layer.cornerRadius = clearButtonView.frame.height / 2
+        plusMinusrButtonView.layer.cornerRadius = plusMinusrButtonView.frame.height / 2
+        percentagerButtonView.layer.cornerRadius = percentagerButtonView.frame.height / 2
+        divisionrButtonView.layer.cornerRadius = divisionrButtonView.frame.height / 2
+        multiplicationrButtonView.layer.cornerRadius = multiplicationrButtonView.frame.height / 2
+        minusrButtonView.layer.cornerRadius = minusrButtonView.frame.height / 2
+        plusrButtonView.layer.cornerRadius = plusrButtonView.frame.height / 2
+        equalrButtonView.layer.cornerRadius = equalrButtonView.frame.height / 2
+        zerorButtonView.layer.cornerRadius = zerorButtonView.frame.height / 2
+        commarButtonView.layer.cornerRadius = commarButtonView.frame.height / 2
+        onerButtonView.layer.cornerRadius = onerButtonView.frame.height / 2
+        tworButtonView.layer.cornerRadius = tworButtonView.frame.height / 2
+        threerButtonView.layer.cornerRadius = threerButtonView.frame.height / 2
+        fourrButtonView.layer.cornerRadius = fourrButtonView.frame.height / 2
+        fiverButtonView.layer.cornerRadius = fiverButtonView.frame.height / 2
+        sixrButtonView.layer.cornerRadius = sixrButtonView.frame.height / 2
+        sevenrButtonView.layer.cornerRadius = sevenrButtonView.frame.height / 2
+        eightrButtonView.layer.cornerRadius = eightrButtonView.frame.height / 2
+        ninerButtonView.layer.cornerRadius = ninerButtonView.frame.height / 2
         resultLabelOutlet.text =  ""
     }
     
     @IBAction func zeroButtonPressed(_ sender: Any) {
     }
     @IBAction func oneButtonPressed(_ sender: Any) {
+        value0 *= 10
+        value0 += 1
+        resultLabelOutlet.text = "\(Int(value0))"
     }
     @IBAction func twoButtonPressed(_ sender: Any) {
     }
@@ -94,8 +100,5 @@ class ViewController: UIViewController {
     }
     @IBAction func equalButtonPressed(_ sender: Any) {
     }
-    
-    
-    
 }
 
