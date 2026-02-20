007c5a6eab692478ca092b5db38137c32b1f1973
VintusS <dragonmd2003@gmail.com>
2026-02-20 13:44:19 +0200

    cleanup hardcoded roundings

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -36,26 +36,60 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        clearButtonView.layer.cornerRadius = clearButtonView.frame.height / 2
-        plusMinusrButtonView.layer.cornerRadius = plusMinusrButtonView.frame.height / 2
-        percentagerButtonView.layer.cornerRadius = percentagerButtonView.frame.height / 2
-        divisionrButtonView.layer.cornerRadius = divisionrButtonView.frame.height / 2
-        multiplicationrButtonView.layer.cornerRadius = multiplicationrButtonView.frame.height / 2
-        minusrButtonView.layer.cornerRadius = minusrButtonView.frame.height / 2
-        plusrButtonView.layer.cornerRadius = plusrButtonView.frame.height / 2
-        equalrButtonView.layer.cornerRadius = equalrButtonView.frame.height / 2
-        zerorButtonView.layer.cornerRadius = zerorButtonView.frame.height / 2
-        commarButtonView.layer.cornerRadius = commarButtonView.frame.height / 2
-        onerButtonView.layer.cornerRadius = onerButtonView.frame.height / 2
-        tworButtonView.layer.cornerRadius = tworButtonView.frame.height / 2
-        threerButtonView.layer.cornerRadius = threerButtonView.frame.height / 2
-        fourrButtonView.layer.cornerRadius = fourrButtonView.frame.height / 2
-        fiverButtonView.layer.cornerRadius = fiverButtonView.frame.height / 2
-        sixrButtonView.layer.cornerRadius = sixrButtonView.frame.height / 2
-        sevenrButtonView.layer.cornerRadius = sevenrButtonView.frame.height / 2
-        eightrButtonView.layer.cornerRadius = eightrButtonView.frame.height / 2
-        ninerButtonView.layer.cornerRadius = ninerButtonView.frame.height / 2
+//        clearButtonView.layer.cornerRadius = clearButtonView.frame.height / 2
+//        plusMinusrButtonView.layer.cornerRadius = plusMinusrButtonView.frame.height / 2
+//        percentagerButtonView.layer.cornerRadius = percentagerButtonView.frame.height / 2
+//        divisionrButtonView.layer.cornerRadius = divisionrButtonView.frame.height / 2
+//        multiplicationrButtonView.layer.cornerRadius = multiplicationrButtonView.frame.height / 2
+//        minusrButtonView.layer.cornerRadius = minusrButtonView.frame.height / 2
+//        plusrButtonView.layer.cornerRadius = plusrButtonView.frame.height / 2
+//        equalrButtonView.layer.cornerRadius = equalrButtonView.frame.height / 2
+//        zerorButtonView.layer.cornerRadius = zerorButtonView.frame.height / 2
+//        commarButtonView.layer.cornerRadius = commarButtonView.frame.height / 2
+//        onerButtonView.layer.cornerRadius = onerButtonView.frame.height / 2
+//        tworButtonView.layer.cornerRadius = tworButtonView.frame.height / 2
+//        threerButtonView.layer.cornerRadius = threerButtonView.frame.height / 2
+//        fourrButtonView.layer.cornerRadius = fourrButtonView.frame.height / 2
+//        fiverButtonView.layer.cornerRadius = fiverButtonView.frame.height / 2
+//        sixrButtonView.layer.cornerRadius = sixrButtonView.frame.height / 2
+//        sevenrButtonView.layer.cornerRadius = sevenrButtonView.frame.height / 2
+//        eightrButtonView.layer.cornerRadius = eightrButtonView.frame.height / 2
+//        ninerButtonView.layer.cornerRadius = ninerButtonView.frame.height / 2
         resultLabelOutlet.text =  ""
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        styleButtons()
+    }
+    
+    private func styleButtons() {
+        let buttons = [
+            clearButtonView,
+            plusMinusrButtonView,
+            percentagerButtonView,
+            divisionrButtonView,
+            multiplicationrButtonView,
+            minusrButtonView,
+            plusrButtonView,
+            equalrButtonView,
+            zerorButtonView,
+            commarButtonView,
+            onerButtonView,
+            tworButtonView,
+            threerButtonView,
+            fourrButtonView,
+            fiverButtonView,
+            sixrButtonView,
+            sevenrButtonView,
+            eightrButtonView,
+            ninerButtonView
+        ]
+
+        buttons.forEach {
+            $0?.layer.cornerRadius = ($0?.frame.height ?? 0) / 2
+            $0?.clipsToBounds = true
+        }
     }
     
     func stopAllModificationButtons(){
