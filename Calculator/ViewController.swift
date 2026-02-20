5289deafc4c1c9f176b1c9d9159e6ca994598394
VintusS <dragonmd2003@gmail.com>
2026-02-20 13:46:20 +0200

    display methods

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -36,26 +36,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        clearButtonView.layer.cornerRadius = clearButtonView.frame.height / 2
-//        plusMinusrButtonView.layer.cornerRadius = plusMinusrButtonView.frame.height / 2
-//        percentagerButtonView.layer.cornerRadius = percentagerButtonView.frame.height / 2
-//        divisionrButtonView.layer.cornerRadius = divisionrButtonView.frame.height / 2
-//        multiplicationrButtonView.layer.cornerRadius = multiplicationrButtonView.frame.height / 2
-//        minusrButtonView.layer.cornerRadius = minusrButtonView.frame.height / 2
-//        plusrButtonView.layer.cornerRadius = plusrButtonView.frame.height / 2
-//        equalrButtonView.layer.cornerRadius = equalrButtonView.frame.height / 2
-//        zerorButtonView.layer.cornerRadius = zerorButtonView.frame.height / 2
-//        commarButtonView.layer.cornerRadius = commarButtonView.frame.height / 2
-//        onerButtonView.layer.cornerRadius = onerButtonView.frame.height / 2
-//        tworButtonView.layer.cornerRadius = tworButtonView.frame.height / 2
-//        threerButtonView.layer.cornerRadius = threerButtonView.frame.height / 2
-//        fourrButtonView.layer.cornerRadius = fourrButtonView.frame.height / 2
-//        fiverButtonView.layer.cornerRadius = fiverButtonView.frame.height / 2
-//        sixrButtonView.layer.cornerRadius = sixrButtonView.frame.height / 2
-//        sevenrButtonView.layer.cornerRadius = sevenrButtonView.frame.height / 2
-//        eightrButtonView.layer.cornerRadius = eightrButtonView.frame.height / 2
-//        ninerButtonView.layer.cornerRadius = ninerButtonView.frame.height / 2
-        resultLabelOutlet.text =  ""
+        resultLabelOutlet.text =  "0"
     }
     
     override func viewDidLayoutSubviews() {
@@ -92,15 +73,24 @@ class ViewController: UIViewController {
         }
     }
     
+    private func currentValue() -> Double {
+        Double(resultLabelOutlet.text ?? "0") ?? 0
+    }
+
+    private func setDisplay(_ value: Double) {
+        resultLabelOutlet.text = NumberFormatterService.format(value)
+    }
+
+    private func setDisplayText(_ text: String) {
+        resultLabelOutlet.text = text
+    }
+    
     func stopAllModificationButtons(){
         commaIsUsed = false
     }
     
     func operatorPressed(_ operation: Operation) {
-        guard let text = resultLabelOutlet.text,
-              let value = Double(text) else { return }
-
-        calculatorEngine.inputOperation(operation, value: value)
+        calculatorEngine.inputOperation(operation, value: currentValue())
         isTypingNumber = false
         commaIsUsed = false
     }
@@ -181,13 +171,8 @@ class ViewController: UIViewController {
 
     
     @IBAction func equalButtonPressed(_ sender: Any) {
-        if let text = resultLabelOutlet.text,
-           let value = Double(text) {
-
-            let result = calculatorEngine.calculateResult(with: value)
-
-            resultLabelOutlet.text = NumberFormatterService.format(result)
-        }
+        let result = calculatorEngine.calculateResult(with: currentValue())
+        setDisplay(result)
 
         isTypingNumber = false
         stopAllModificationButtons()
