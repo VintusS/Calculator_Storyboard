091fb8d663551aa4df6c323a608b0cf4e42f9bc4
Dragomir Mindrescu <dragonmd2003@gmail.com>
2026-02-20 13:20:02 +0200

    Optimization Part 1 (#3)

---
+++ Calculator/Services/CalculatorEngine.swift
@@ -0,0 +1,59 @@
+//
+//  CalculatorEngine.swift
+//  Calculator
+//
+//  Created by Dragosh on 18.02.2026.
+//
+
+import Foundation
+import UIKit
+
+enum Operation {
+    case none
+    case add
+    case subtract
+    case multiply
+    case divide
+}
+
+struct CalculatorEngine {
+
+    private var storedValue: Double = 0
+    private var pendingOperation: Operation = .none
+    private var hasStoredValue = false
+
+    mutating func inputOperation(_ operation: Operation, value: Double) {
+        if hasStoredValue {
+            storedValue = perform(pendingOperation, val0: storedValue, val1: value)
+        } else {
+            storedValue = value
+            hasStoredValue = true
+        }
+        
+        pendingOperation = operation
+    }
+
+    mutating func calculateResult(with value: Double) -> Double {
+        let result = perform(pendingOperation, val0: storedValue, val1: value)
+        storedValue = result
+        pendingOperation = .none
+        hasStoredValue = true
+        return result
+    }
+    
+    private func perform(_ operation: Operation, val0: Double, val1: Double) -> Double {
+        switch operation {
+            case .add: return val0 + val1
+            case .subtract: return val0 - val1
+            case .multiply: return val0 * val1
+            case .divide: return val1 == 0 ? val0 : val0 / val1
+            case .none: return 0
+        }
+    }
+
+    mutating func reset() {
+        storedValue = 0
+        pendingOperation = .none
+        hasStoredValue = false
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -30,14 +30,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var eightrButtonView: UIButton!
     @IBOutlet weak var ninerButtonView: UIButton!
     
-    var value0: Float = 0
-    var value1: Float = 0
-    var commaIsUsed: Bool = false
-    var divisionIsPressed: Bool = false
-    var multiplicationIsPressed: Bool = false
-    var subtractionIsPressed: Bool = false
-    var additionIsPressed: Bool = false
-    var operationCount: Int = 0
+    private var commaIsUsed: Bool = false
+    private var isTypingNumber = false
+    private var calculatorEngine = CalculatorEngine()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,187 +59,35 @@ class ViewController: UIViewController {
     }
     
     func stopAllModificationButtons(){
-        if subtractionIsPressed{
-            let tempColor = minusrButtonView.backgroundColor
-            minusrButtonView.backgroundColor = minusrButtonView.titleLabel?.textColor
-            minusrButtonView.setTitleColor(tempColor, for: .normal)
-            subtractionIsPressed = false
-        }
-        if additionIsPressed{
-            let tempColor = plusrButtonView.backgroundColor
-            plusrButtonView.backgroundColor = plusrButtonView.titleLabel?.textColor
-            plusrButtonView.setTitleColor(tempColor, for: .normal)
-            additionIsPressed = false
-        }
-        if divisionIsPressed{
-            let tempColor = divisionrButtonView.backgroundColor
-            divisionrButtonView.backgroundColor = divisionrButtonView.titleLabel?.textColor
-            divisionrButtonView.setTitleColor(tempColor, for: .normal)
-            divisionIsPressed = false
-        }
-        if multiplicationIsPressed{
-            let tempColor = multiplicationrButtonView.backgroundColor
-            multiplicationrButtonView.backgroundColor = multiplicationrButtonView.titleLabel?.textColor
-            multiplicationrButtonView.setTitleColor(tempColor, for: .normal)
-            multiplicationIsPressed = false
-        }
+        commaIsUsed = false
+    }
+    
+    func operatorPressed(_ operation: Operation) {
+        guard let text = resultLabelOutlet.text,
+              let value = Double(text) else { return }
+
+        calculatorEngine.inputOperation(operation, value: value)
+        isTypingNumber = false
         commaIsUsed = false
     }
     
     //Max digits in the number = 12 (including floating point)
     
+    @IBAction func digitPressed(_ sender: UIButton) {
+        guard let digit = sender.currentTitle else { return }
+        clearButtonView.setTitle("C", for: .normal)
+        
+        if !isTypingNumber {
+            resultLabelOutlet.text = digit
+            isTypingNumber = true
+        } else {
+            if let count = resultLabelOutlet.text?.count, count < 12 {
+                resultLabelOutlet.text?.append(digit)
+            }
+        }
+    }
     
-    @IBAction func zeroButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if digitsCount != nil {
-            if digitsCount! <= 12, digitsCount! > 0{
-                resultLabelOutlet.text?.append("0")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
-    @IBAction func oneButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if resultLabelOutlet.text == "0"{
-            resultLabelOutlet.text = ""
-        }
-        if digitsCount != nil {
-            if digitsCount! <= 12 {
-                resultLabelOutlet.text?.append("1")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
-    @IBAction func twoButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if resultLabelOutlet.text == "0"{
-            resultLabelOutlet.text = ""
-        }
-        if digitsCount != nil {
-            if digitsCount! <= 12 {
-                resultLabelOutlet.text?.append("2")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
-    @IBAction func threeButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if resultLabelOutlet.text == "0"{
-            resultLabelOutlet.text = ""
-        }
-        if digitsCount != nil {
-            if digitsCount! <= 12 {
-                resultLabelOutlet.text?.append("3")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
-    @IBAction func fourButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if resultLabelOutlet.text == "0"{
-            resultLabelOutlet.text = ""
-        }
-        if digitsCount != nil {
-            if digitsCount! <= 12 {
-                resultLabelOutlet.text?.append("4")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
-    @IBAction func fiveButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if resultLabelOutlet.text == "0"{
-            resultLabelOutlet.text = ""
-        }
-        if digitsCount != nil {
-            if digitsCount! <= 12 {
-                resultLabelOutlet.text?.append("5")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
-    @IBAction func sixButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if resultLabelOutlet.text == "0"{
-            resultLabelOutlet.text = ""
-        }
-        if digitsCount != nil {
-            if digitsCount! <= 12 {
-                resultLabelOutlet.text?.append("6")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
-    @IBAction func sevenButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if resultLabelOutlet.text == "0"{
-            resultLabelOutlet.text = ""
-        }
-        if digitsCount != nil {
-            if digitsCount! <= 12 {
-                resultLabelOutlet.text?.append("7")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
-    @IBAction func eightButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if resultLabelOutlet.text == "0"{
-            resultLabelOutlet.text = ""
-        }
-        if digitsCount != nil {
-            if digitsCount! <= 12 {
-                resultLabelOutlet.text?.append("8")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
-    @IBAction func nineButtonPressed(_ sender: Any) {
-        clearButtonView.setTitle("C", for: .normal)
-        let digitsCount = resultLabelOutlet.text?.count
-        if resultLabelOutlet.text == "0"{
-            resultLabelOutlet.text = ""
-        }
-        if digitsCount != nil {
-            if digitsCount! <= 12 {
-                resultLabelOutlet.text?.append("9")
-            }
-        }
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
-    }
     @IBAction func commaButtonPressed(_ sender: Any) {
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-        }
         clearButtonView.setTitle("C", for: .normal)
         let digitsCount = resultLabelOutlet.text?.count
         if resultLabelOutlet.text == "0"{
@@ -260,13 +103,12 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func clearButtonPressed(_ sender: Any) {
-        value0 = 0
-        value1 = 0
+        calculatorEngine.reset()
         commaIsUsed = false
+        isTypingNumber = false
         clearButtonView.setTitle("AC", for: .normal)
         resultLabelOutlet.text = "0"
         stopAllModificationButtons()
-        operationCount = 0
         
     }
     @IBAction func oppositeButtonPressed(_ sender: Any) {
@@ -278,97 +120,43 @@ class ViewController: UIViewController {
             } else {
                 resultLabelOutlet.text = "\(floatValue)"
             }
-            value1 = floatValue
         }
     }
     @IBAction func percentageButtonPressed(_ sender: Any) {
-        if let value = resultLabelOutlet.text {
-            value1 = (value as NSString).floatValue
-            value1 /= 100
-            resultLabelOutlet.text = "\(value1)"
+        if let text = resultLabelOutlet.text,
+           let value = Double(text){
+            let result = value / 100
+            resultLabelOutlet.text = "\(result)"
         }
     }
-    @IBAction func divisionButtonPressed(_ sender: Any) {
-        stopAllModificationButtons()
-        let tempColor = divisionrButtonView.backgroundColor
-        divisionrButtonView.backgroundColor = divisionrButtonView.titleLabel?.textColor
-        divisionrButtonView.setTitleColor(tempColor, for: .normal)
-        if !subtractionIsPressed {
-            if let value = resultLabelOutlet.text {
-                value1 = (value as NSString).floatValue
-            }
-            resultLabelOutlet.text = ""
-            operationCount += 1
-        }
-        divisionIsPressed = true
-        value0 = value1
-        value1 = 0
-    }
-    @IBAction func multiplicationButtonPressed(_ sender: Any) {
-        stopAllModificationButtons()
-        let tempColor = multiplicationrButtonView.backgroundColor
-        multiplicationrButtonView.backgroundColor = multiplicationrButtonView.titleLabel?.textColor
-        multiplicationrButtonView.setTitleColor(tempColor, for: .normal)
-        if !multiplicationIsPressed {
-            if let value = resultLabelOutlet.text {
-                value1 = (value as NSString).floatValue
-            }
-            resultLabelOutlet.text = ""
-            operationCount += 1
-        }
-        multiplicationIsPressed = true
-        value0 = value1
-        value1 = 0
-    }
-    @IBAction func minusButtonPressed(_ sender: Any) {
-        stopAllModificationButtons()
-        let tempColor = minusrButtonView.backgroundColor
-        minusrButtonView.backgroundColor = minusrButtonView.titleLabel?.textColor
-        minusrButtonView.setTitleColor(tempColor, for: .normal)
-        if !subtractionIsPressed {
-            if let value = resultLabelOutlet.text {
-                value1 = (value as NSString).floatValue
-            }
-            resultLabelOutlet.text = ""
-            operationCount += 1
-        }
-        subtractionIsPressed = true
-        value0 = value1
-        value1 = 0
-    }
+
     @IBAction func plusButtonPressed(_ sender: Any) {
-        stopAllModificationButtons()
-        let tempColor = plusrButtonView.backgroundColor
-        plusrButtonView.backgroundColor = plusrButtonView.titleLabel?.textColor
-        plusrButtonView.setTitleColor(tempColor, for: .normal)
-        if !additionIsPressed {
-            if let value = resultLabelOutlet.text {
-                value1 = (value as NSString).floatValue
-            }
-            resultLabelOutlet.text = ""
-            operationCount += 1
-        }
-        additionIsPressed = true
-        value0 = value1
-        value1 = 0
+        operatorPressed(.add)
     }
+    
+    @IBAction func minusButtonPressed(_ sender: Any) {
+        operatorPressed(.subtract)
+    }
+
+    @IBAction func multiplicationButtonPressed(_ sender: Any) {
+        operatorPressed(.multiply)
+    }
+
+    @IBAction func divisionButtonPressed(_ sender: Any) {
+        operatorPressed(.divide)
+    }
+
+    
     @IBAction func equalButtonPressed(_ sender: Any) {
-        if additionIsPressed {
-            value0 += value1
-        } else if subtractionIsPressed {
-            value0 -= value1
-        } else if divisionIsPressed {
-            value0 /= value1
-        } else if multiplicationIsPressed {
-            value0 *= value1
+        if let text = resultLabelOutlet.text,
+           let value = Double(text) {
+
+            let result = calculatorEngine.calculateResult(with: value)
+
+            resultLabelOutlet.text = NumberFormatterService.format(result)
         }
-        value1 = 0
+
+        isTypingNumber = false
         stopAllModificationButtons()
-        if Float(Int(value0)) == value0 {
-            resultLabelOutlet.text = "\(Int(value0))"
-        } else {
-            resultLabelOutlet.text = "\(value0)"
-        }
     }
 }
-
